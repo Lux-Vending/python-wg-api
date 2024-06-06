0ac3ed250c1170db7b40b29f7c92cece4365026d
Peter Sjoberg <peter@bitaccess.co>
2024-06-06 11:52:07 -0400

    renamed app, python_wireguard -> python_wg_api

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 '''
-Used for building the python_wireguard package.
+Used for building the python_wg_api package.
 '''
 from pathlib import Path
 import os
@@ -10,15 +10,15 @@ long_description = (this_directory / "README.md").read_text()
 
 version = os.environ.get('RELEASE_VERSION')
 
-setup(name='python_wireguard',
+setup(name='python_wg_api',
       version=version,
       description='A python wrapper for controlling Wireguard',
       long_description=long_description,
       long_description_content_type="text/markdown",
-      url='https://github.com/jarnoaxel/python-wireguard',
+      url='https://github.com/jarnoaxel/python-wg-api',
       author='jarnoaxel',
       license='GPL-3.0-or-later',
-      packages=['python_wireguard'],
+      packages=['python_wg_api'],
       include_package_data=True,
       keywords=['vpn', 'wireguard'],
       zip_safe=False)
