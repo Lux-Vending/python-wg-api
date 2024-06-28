078447961f192e62749b5395d0a5297f9bd6a26f
Peter Sjoberg <peter@bitaccess.co>
2024-06-28 16:02:27 -0400

    fixed lint errors

--- test_get_devices.py
+++ test_get_devices.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python3
+'''
+Simple program to test wireguard.get_devices
+'''
 
-from python_wireguard import wireguard
 import json
+from python_wireguard import wireguard
 
 for max_size in range(-2,5):
     print(f"maxsize={max_size}")
-    res,rc=wireguard.get_devices(max_size)
-    print(f" maxsize {max_size} return: result='{res}' length result={len(res)}, return code={rc}")
+    RESULT,rc=wireguard.get_devices(max_size)
+    # pylint: disable=line-too-long
+    print(f" maxsize {max_size} return: result='{RESULT}' length result={len(RESULT)}, return code={rc}")
 
-mem_cache_size=4096
+MEM_CACHE_SIZE=4096
 
 while True:
-    print(f"mem_cache_size={mem_cache_size}")
-    res,ret=wireguard.get_devices(mem_cache_size)
+    print(f"MEM_CACHE_SIZE={MEM_CACHE_SIZE}")
+    RESULT,ret=wireguard.get_devices(MEM_CACHE_SIZE)
     if ret >= 0:
         break
-    mem_cache_size = int(mem_cache_size*1.1+1)
-    print(f"  mem_cache_size changed, new size is ={mem_cache_size}, res len={len(res)}, return code was {ret}")
+    MEM_CACHE_SIZE = int(MEM_CACHE_SIZE*1.1+1)
+    # pylint: disable=line-too-long
+    print(f"  MEM_CACHE_SIZE changed, new size is ={MEM_CACHE_SIZE}, result len={len(RESULT)}, return code was {ret}")
 
-print(f"length = {len(res)}")
-print(res)
+print(f"length = {len(RESULT)}")
+print(RESULT)
 print()
-device_list=json.loads(res)
-res=""
+device_list=json.loads(RESULT)
+RESULT=""
 for dev in device_list:
     print(f">>>>>>>>>>>>>>>>{dev}")
     print(json.dumps(device_list[dev],indent=2))
 
-#print(json.dumps(json.loads(res),indent=2))
+#print(json.dumps(json.loads(RESULT),indent=2))
